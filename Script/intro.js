169361d6de08341d446d75a8c6056966cd15f42d
Gearleur <alexandretei13@gmail.com>
2021-11-25 10:17:18 +0100

    up

--- Script/intro.js
+++ Script/intro.js
@@ -6,6 +6,7 @@
 const result = document.getElementById('result')
 const nomInp = document.getElementById('nom')
 const goBtn = document.getElementById('go')
+const hello = document.getElementById('hello')
 
 result.textContent = 'Ceci est un test'
 
@@ -13,5 +14,21 @@ let countClick = 0
 goBtn.addEventListener('click', function click(ev) {
     countClick++
     result.textContent = "Clicks : " + countClick
+})
+
+/*Au click sur 'GO', si on a une valeur, la mettre
+dans la div d'id hello
+sinon, afficher une erreur dans la div d'id' hello */
+goBtn.addEventListener('click', function click(ev) {
+    const valeur = nomInp.value
+
+    if (!valeur || valeur.trim() === '') {
+        hello.textContent = "Saisissez une valeur"
+        return
+    }
+    hello.innerHTML = 'Hello <b>' + valeur.trim() + '</b>'
+
+
+    
 })
 
