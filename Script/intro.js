bf8323ef71e4c618d4a97a0a3f2f0e86f55005ee
Gearleur <alexandretei13@gmail.com>
2021-11-25 09:33:01 +0100

    up

--- Script/intro.js
+++ Script/intro.js
@@ -1,2 +1,17 @@
-let nom = prompt("Entrer votre nom")
-alert(`Hello' ${nom}`)
+//window.alert('hello' + nom)
+
+//let nom = prompt("Entrer votre nom")
+//alert(`Hello' ${nom}`)
+
+const result = document.getElementById('result')
+const nomInp = document.getElementById('nom')
+const goBtn = document.getElementById('go')
+
+result.textContent = 'Ceci est un test'
+
+let countClick = 0
+goBtn.addEventListener('click', function click(ev) {
+    countClick++
+    result.textContent = "Clicks : " + countClick
+})
+
