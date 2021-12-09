407bc308ee4b94ebaedbdc800550082cfda74593
Gearleur <alexandretei13@gmail.com>
2021-12-09 10:45:32 +0100

    commande js

--- Script/commande.js
+++ Script/commande.js
@@ -1,16 +1,27 @@
 const form = document.getElementById('form')
 const subject = document.getElementById('subject')
 const copie = document.getElementById('copie')
-const bpody = document.getElementById('body')
+const body = document.getElementById('body')
+const subjectError = document.getElementById('subjectError')
+const bodyError = document.getElementById('bodyError')
 
 form.addEventListener('submit', function submit(ev) {
     if (subject.value.length < 3) {
-        ev.preventDefault();
-        alert ('Le sujet doit contenir trois caractère')
+        ev.preventDefault()
+        subjectError.innerText = 'Le sujet doit contenir trois caractères minimum'
+    } else {
+        subjectError.innerText = ''
     }
 
     if (body.value.length < 20) {
         ev.preventDefault()
-        alert ('Le corps du mail doit contenir vinght caractère minimum')
+        bodyError.innerText = 'Le corps du mail doit contenir vingt caractères minimum'
+    } else {
+        bodyError.innerText = ''
     }
 })
+form.addEventListener('reset', function reset() {
+    subjectError.innerText = ''
+    bodyError.innerText = ''
+})
+
